adbfbb0a19679c3b6c7f13c218049a6c44bc4f01
Julia Oliveira <104094569+JuliaOliFranco@users.noreply.github.com>
2023-01-30 15:10:32 -0300

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html lang="pt-br">
+		<head>
+			<title>Teste da Linguagem PHP</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<?php 
+				echo '<h1>Teste da Linguagem HTML (HyperText Markup Language)</h1>';
+				echo 'Autor: Julia Oliveira<br>';
+				echo 'Linkedin: https://www.linkedin.com/in/julia-oliveira-franco-164534238/<br>';
+				echo 'Site: https://github.com/JuliaOliFranco<br>';
+				echo 'Instagram: https://www.instagram.com/juuh__olvr/<br>';
+				echo 'YouTube: youtube.com/BoraParaPratica<br>'; 
+                <img src="./2cb34d287116284a50c6676cf5360b56.jpg" alt="boo e sully">
+			?>
+		</body>
+	</html>
